f181d05addbd0a893acecbc64c9cec1831378576
fangyaohui <2944163240@qq.com>
2024-12-03 20:33:44 +0800

    添加CryptGuard模块

---
+++ CryptGuard/src/main/java/com/example/cryptguard/CryptGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptGuardApplication.class, args);
+    }
+
+}
