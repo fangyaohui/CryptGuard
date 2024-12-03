ad23e27178ec1dcd154cceb141a2670b305d2eb7
fangyaohui <2944163240@qq.com>
2024-12-03 20:30:37 +0800

    初始化项目

---
+++ CrptGuardUseTest/src/main/java/com/example/crptguardusetest/CrptGuardUseTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crptguardusetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrptGuardUseTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrptGuardUseTestApplication.class, args);
+    }
+
+}
---
+++ CrptGuardUseTest/src/test/java/com/example/crptguardusetest/CrptGuardUseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crptguardusetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrptGuardUseTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
